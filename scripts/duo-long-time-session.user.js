7a2bf2e4b0098dc29ad7a4b9a5d50e58c3089962
Maxim Podkolzine <Maxim.Podkolzine@jetbrains.com>
2014-08-22 17:47:52 +0200

    first simple scripts

---
+++ scripts/duo-long-time-session.user.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name       Duolingo Long Time Session Userscript
+// @namespace  https://github.com/maxim5
+// @version    0.2
+// @description  enter something useful
+// @match      https://www.duolingo.com/
+// @copyright  2014+
+// ==/UserScript==
+
+var originalTimingModule = duo.TimingModule;
+
+duo.TimingModule = duo.TimingModule.extend({
+    initialize: function(options) {
+        options.total_time = 300;
+        options.original_total_time = 300;
+        options.long_sentence_bonus = 5;
+        originalTimingModule.prototype.initialize.call(this, options);
+    }
+});
+
+console.log("[Duolingo Long Time Session patch applied]");
---
+++ scripts/duo-time-session-ux.user.js
@@ -0,0 +1,26 @@
+// ==UserScript==
+// @name       Duolingo Time Session UX Userscript
+// @namespace  https://github.com/maxim5
+// @version    0.2
+// @description  enter something useful
+// @match      https://www.duolingo.com/
+// @copyright  2014+
+// ==/UserScript==
+
+var originalTimingModule = duo.TimingModule;
+
+duo.TimingModule = duo.TimingModule.extend({
+    initialize: function(options) {
+        originalTimingModule.prototype.initialize.call(this, options);
+      	this.model.bind("continue", this.resume, this);
+    },
+    
+    // A copy that does not call `this.resume();`.
+    graded: function() {
+        var solution = this.model.getSubmittedSolution(),
+            session = solution.get("session_element");
+        this.extra && solution.get("correct") && (this.total_time += 1E3 * this.model.calculateExtraTime(session));
+    }
+});
+
+console.log("[Duolingo Time Session UX patch applied]");
